49531d615e592e8fb01d79dbe3efe96aec365a82
Patriarh42 <efimas2010@gmail.com>
2026-05-09 18:18:48 +0300

    Update models.py

--- src/database/models.py
+++ src/database/models.py
@@ -1,1 +1,11 @@
-
+class User:
+    def __init__(self, email, username, password):
+        self.email = email
+        self.username = username
+        self.password = password
+    
+    def to_dict(self):
+        return {
+            "email": self.email,
+            "username": self.username
+        }
